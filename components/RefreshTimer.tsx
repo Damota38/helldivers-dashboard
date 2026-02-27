954140f6b80210629909a8b2b57cfe1948b074b6
Damota38 <a.damota.dw@gmail.com>
2026-02-27 14:52:33 +0100

    update: refreshTimer

--- components/RefreshTimer.tsx
+++ components/RefreshTimer.tsx
@@ -19,11 +19,4 @@ export default function RefreshTimer({ intervalSeconds }: Props) {
 
         return () => clearInterval(timer)
     }, [intervalSeconds])
-
-    return (
-        <div className="flex items-center gap-2 text-gray-500 text-xs">
-            <div className="w-2 h-2 rounded-full bg-green-500 animate-pulse" />
-            <span>Actualisation dans {secondsLeft}s</span>
-        </div>
-    )
 }
